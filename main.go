eee92917c8a386df34cc0292797bb6da7d3b28f6
HavokMoobii <jasonmcounter@gmail.com>
2025-11-10 01:13:03 -0600

    Added readiness, metrics, and reset endpoints

--- main.go
+++ main.go
@@ -3,15 +3,30 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
-	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	config := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
+	mux := http.NewServeMux()
+
+	fsHandler := config.middlewareMetricsInc(http.StripPrefix("/app", (http.FileServer(http.Dir(filepathRoot)))))
+	mux.Handle("/app/", fsHandler)
+
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("GET /metrics", config.handlerMetrics)
+	mux.HandleFunc("POST /reset", config.handlerReset)
+	
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
