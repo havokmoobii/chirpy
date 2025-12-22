edbb735d425281bac2db45034950a0fd6d112ca7
HavokMoobii <jasonmcounter@gmail.com>
2025-12-21 20:41:21 -0600

    Added sorting to handler_get

--- api/handlers/handler_chirps_get.go
+++ api/handlers/handler_chirps_get.go
@@ -29,48 +29,46 @@ func (cfg *APIConfig) HandlerChirpsGet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *APIConfig) HandlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
-	s := r.URL.Query().Get("author_id")
-	if s != "" {
-		parsed, err := uuid.Parse(s)
-		if err != nil {
-			respondWithError(w, http.StatusBadRequest, "Couldn't parse request author_id", err)
-			return
-		}
-
-		dbChirps, err := cfg.DB.GetChirpsFromID(r.Context(), parsed)
-		if err != nil {
-			respondWithError(w, http.StatusInternalServerError, "Couldn't get chirps", err)
-			return
-		}
-
-		chirps := make([]Chirp, len(dbChirps))
-
-		for i, dbChirp := range dbChirps {
-			chirps[i].ID = dbChirp.ID
-			chirps[i].CreatedAt = dbChirp.CreatedAt
-			chirps[i].UpdatedAt = dbChirp.UpdatedAt
-			chirps[i].Body = dbChirp.Body
-			chirps[i].UserID = dbChirp.UserID
-		}
-
-		respondWithJSON(w, http.StatusOK, chirps)
-		return
-	}
-	
 	dbChirps, err := cfg.DB.GetChirps(r.Context())
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't get chirps", err)
 		return
 	}
+	
+	parsed := uuid.Nil
+	s := r.URL.Query().Get("author_id")
+	if s != "" {
+		parsed, err = uuid.Parse(s)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Couldn't parse request author_id", err)
+			return
+		}
+	}
 
-	chirps := make([]Chirp, len(dbChirps))
+	chirps := make([]Chirp, 0)
 
-	for i, dbChirp := range dbChirps {
-		chirps[i].ID = dbChirp.ID
-		chirps[i].CreatedAt = dbChirp.CreatedAt
-		chirps[i].UpdatedAt = dbChirp.UpdatedAt
-		chirps[i].Body = dbChirp.Body
-		chirps[i].UserID = dbChirp.UserID
+	for _, dbChirp := range dbChirps {
+		if s != "" && dbChirp.UserID != parsed {
+			continue
+		}
+
+		chirps = append(chirps, Chirp{
+			ID:        dbChirp.ID,
+			CreatedAt: dbChirp.CreatedAt,
+			UpdatedAt: dbChirp.UpdatedAt,
+			Body:      dbChirp.Body,
+			UserID:    dbChirp.UserID,
+		})
+	}
+
+	sortOrder := r.URL.Query().Get("sort")
+	if sortOrder == "desc" {
+		sortedChirps := make([]Chirp, len(chirps))
+		for i := 0; i < len(chirps); i++ {
+			sortedChirps[len(chirps) - (i + 1)] = chirps[i] 
+		}
+		respondWithJSON(w, http.StatusOK, sortedChirps)
+		return
 	}
 
 	respondWithJSON(w, http.StatusOK, chirps)
--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -13,11 +13,6 @@ RETURNING *;
 SELECT * FROM chirps
 ORDER BY created_at ASC;
 
--- name: GetChirpsFromID :many
-SELECT * FROM chirps
-WHERE user_id = $1
-ORDER BY created_at ASC;
-
 -- name: GetChirp :one
 SELECT * FROM chirps
 WHERE id = $1;
